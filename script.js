3d302572f6b0ef3808df56a1403d121af9fc4705
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-06-10 13:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ for (let i = 0; i < allButtons.length; i++) {
 	let currButton = allButtons[i];
 	currButton.addEventListener('click', (e) => {
 		let code = e.target.getAttribute('data-key');
-		console.log(code);
+		
+		playSound(code);
 	});
+}
+
+let soundObj = {
+	65: "https://samplelib.com/lib/preview/mp3/sample-3s.mp3",
+	83: "https://samplelib.com/lib/preview/mp3/sample-3s.mp3",
+	68: "https://samplelib.com/lib/preview/mp3/sample-3s.mp3",
+	70: "https://samplelib.com/lib/preview/mp3/sample-6s.mp3",
+	71: "https://samplelib.com/lib/preview/mp3/sample-6s.mp3",
+	72: "https://samplelib.com/lib/preview/mp3/sample-6s.mp3",
+	74: "https://samplelib.com/lib/preview/mp3/sample-6s.mp3",
+	75: "https://samplelib.com/lib/preview/mp3/sample-6s.mp3",
+	76: "https://samplelib.com/lib/preview/mp3/sample-6s.mp3",
+}
+function playSound(key) {
+	console.log(key)
+	var audio = new Audio(soundObj[key]);
+    audio.play();
 }
