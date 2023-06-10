8d2ea4f4db8557d029a8bf4f13fc5917e1c75111
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-06-10 13:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let allButtons = document.getElementsByTagName('button');
+for (let i = 0; i < allButtons.length; i++) {
+	let currButton = allButtons[i];
+	currButton.addEventListener('click', (e) => {
+		let code = e.target.getAttribute('data-key');
+		console.log(code);
+	});
+}
